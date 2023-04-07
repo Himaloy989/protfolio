78deea53757d65188b2f20754e50da1915ff1960
Himaloy Datta. (Himu) <121860658+Himaloy989@users.noreply.github.com>
2023-04-07 22:20:19 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multi-text", {
+    strings: ["Frontend Developer", " FREELANCER", "Youtuber", "GAMMER"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop: true,
+})
